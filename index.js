f3fc205ad313c55f638a54536c92bb14e2061231
a3fg7k <127309680+a3fg7k@users.noreply.github.com>
2023-03-08 23:07:06 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const express = require('express'),
+    app = express(),
+    puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+      args: ['--no-sandbox']
+    });
+  console.log('browser launched')
+  const page = await browser.newPage();
+  console.log('created page')
+  await page.goto('https://now.gg/play/yostar-limited/2211/arknights');
+  
+  console.log('navigated')
+  
+  app.get("/", async (request, response) => {
+    console.log('scre start')
+    try {
+
+      await page.screenshot({path: __dirname+'/public/puppeteer.png'});
+      response.sendFile(__dirname+'/public/puppeteer.png');
+    } catch (error) {
+      console.log(error);
+    }
+    console.log('scre end')
+  });
+
+  var listener = app.listen(process.env.PORT, function () {
+    console.log('Your app is listening on port ' + listener.address().port);
+  });
+})()
