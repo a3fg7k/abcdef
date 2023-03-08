43dca87ba0df2ea944803c8a743b30d3b784a26e
a3fg7k <127309680+a3fg7k@users.noreply.github.com>
2023-03-08 23:43:11 +1100

    Update index.js

--- index.js
+++ index.js
@@ -24,32 +24,34 @@ ls.on("close", code => {
     (async () => {
     
     
-  const browser = await puppeteer.launch({
-      args: ['--no-sandbox']
-    });
-  console.log('browser launched')
-  const page = await browser.newPage();
-  console.log('created page')
-  await page.goto('https://now.gg/play/yostar-limited/2211/arknights');
-  
-  console.log('navigated')
-  
-  app.get("/", async (request, response) => {
-    console.log('scre start')
-    try {
+    //   const browser = await puppeteer.launch({
+    //       args: ['--no-sandbox']
+    //     });
+    //   console.log('browser launched')
+    //   const page = await browser.newPage();
+    //   console.log('created page')
+    //   await page.goto('https://now.gg/play/yostar-limited/2211/arknights');
 
-      await page.screenshot({path: __dirname+'/public/puppeteer.png'});
-      response.sendFile(__dirname+'/public/puppeteer.png');
-    } catch (error) {
-      console.log(error);
-    }
-    console.log('scre end')
-  });
+    //   console.log('navigated')
 
-  var listener = app.listen(process.env.PORT, function () {
-    console.log('Your app is listening on port ' + listener.address().port);
-  });
-})()
+    //   app.get("/", async (request, response) => {
+    //     console.log('scre start')
+    //     try {
+
+    //       await page.screenshot({path: __dirname+'/public/puppeteer.png'});
+    //       response.sendFile(__dirname+'/public/puppeteer.png');
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //     console.log('scre end')
+    //   });
+
+    //   var listener = app.listen(process.env.PORT, function () {
+    //     console.log('Your app is listening on port ' + listener.address().port);
+    //   });
+    
+    
+    })()
 });
 
 
